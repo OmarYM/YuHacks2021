e496a6b963a94d813b6ef51acc7041f4ca40c8c6
Omar Mohamud <39844000+Omar11218@users.noreply.github.com>
2021-02-13 14:16:43 -0500

    added counting animations

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -3,7 +3,10 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", function(req, res) {
-  res.render("home/index");
+    res.render("home/index", {
+        count1: 1000,
+        count2: 200000
+    });
 });
 
 module.exports = router;
